96e6ef96865a40dd49363277ce2226c759cb914f
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-02-07 15:42:21 -0800

    Add register logic

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const users = require("./routes/api/users");
+const orders = require("./routes/api/orders");
+const restaurants = require("./routes/api/restaurants");
+const samples = require("./routes/api/samples");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/users", users);
+app.use("/api/orders", orders);
+app.use("/api/retaurants", restaurants);
+app.use("/api/samples", samples);
+
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    require: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+const User = mongoose.model('users', UserSchema);
+module.exports = User
