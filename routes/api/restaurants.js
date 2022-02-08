f38e9729207ce39807878206325b2fa8c9ba28b8
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-02-08 14:10:44 -0800

    adding validations for res and for samples

--- routes/api/restaurants.js
+++ routes/api/restaurants.js
@@ -29,6 +29,106 @@ router.get(
   }
 )
 
+router.post("/register", (req, res) => {
+
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  Restaurant.findOne({ email: req.body.email }).then(stylist => {
+    if (stylist) {
+      errors.email = "Email has already been registered";
+      return res.status(400).json(errors);
+    } else {
+      Restaurant.findOne({ restaurantName: req.body.restaurantName }).then(restaurant => {
+        if (restaurant) {
+          errors.handle = "Restaurant name has already been taken";
+          return res.status(400).json(errors);
+        }
+      })
+      const newRestaurant = new Restaurant({
+        restaurantOwner: req.body.restaurantOwner,
+        restaurantName: req.body.restaurantName,
+        email: req.body.email,
+        password: req.body.password,
+        profileType: req.body.profileType,
+        phoneNumber: req.body.phoneNumber,
+        address: req.body.address,
+        averageRating: req.body.averageRating,
+        imageUrl: ""
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newStylist.password, salt, (err, hash) => {
+          if (err) throw err;
+          newRestaurant.password = hash;
+          newRestaurant
+            .save()
+            .then(restaurant => {
+              const payload = { id: restaurant.id, restaurantName: restaurant.restaurantName };
+
+              jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                res.json({
+                  success: true,
+                  token: "Bearer " + token
+                });
+              });
+            })
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+
+router.post("/login", (req, res) => {
+
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  Restaurant.findOne({ email }).then(restaurant => {
+    if (!restaurant) {
+      errors.email = "This restaurant does not exist";
+      return res.status(400).json(errors);
+    }
+
+    bcrypt.compare(password, restaurant.password).then(isMatch => {
+      if (isMatch) {
+        const payload = { id: restaurant.id, email: restaurant.email, restaurantName: restaurant.restaurantName, restaurantOwner: restaurant.restaurantOwner, address: restaurant.address, phoneNumber: restaurant.phoneNumber, imageUrl: restaurant.imageUrl };
+
+        jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+          res.json({
+            success: true,
+            token: "Bearer " + token
+          });
+        });
+      } else {
+        errors.password = "Incorrect password";
+        return res.status(400).json(errors);
+      }
+    });
+  });
+});
+
+router.patch(
+  "/:id",
+  // passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+
+    Restaurant.findByIdAndUpdate((req.params.id), req.body, { new: true })
+      .then(restaurant => res.json(restaurant))
+      .catch(err => res.status(404).json({ norestaurantfound: "No restaurant found by that Id" }))
+  }
+)
 
 
 
