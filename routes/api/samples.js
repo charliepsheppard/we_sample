c88ab4a99301fb42c8bfc2cc0cb1e5bb020bdb4d
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-02-08 12:07:20 -0800

    added in backend crud for samples

--- routes/api/samples.js
+++ routes/api/samples.js
@@ -1,6 +1,80 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require('passport');
+const Sample = require("../../models/Sample");
+const validateSample = require("../../validation/sample")
 
 router.get("/test", (req, res) => res.json({ msg: "This is the samples route" }));
+
+router.get(
+  "/",
+  (req, res) => {
+    Sample.find()
+      .then(samples => res.json(samples))
+      .catch(err => res.status(404).json({ nosamplesfound: "No samples found" }));
+  }
+)
+
+router.get(
+  "/:id",
+  (req, res) => {
+    Sample.findById(req.params.id)
+      .then(sample => res.json(sample))
+      .catch(err => res.status(404).json({ nosamplefound: "No sample found by that Id" }))
+  }
+)
+
+router.get(
+  "/restaurant/:restaurantId",
+  (req, res) => {
+    Style.find({restaurantId: req.params.restaurantId})
+      .then(samples => res.json(samples))
+      .catch(err => res.status(404).json({ nosamplesfound: "No samples found by that Id"}))
+  }
+)
+
+router.post(
+  "/",
+  // passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+
+    const { errors, isValid } = validateSample(req.body);
+    if (!isValid) {
+      return res.status(400).json(errors)
+    }
+
+    const newSample = new Sample({
+      sampleItem: req.body.sampleItem,
+      description: req.body.description,
+      price: req.body.price,
+      restaurantId: req.body.restaurantId,
+      imageUrl: req.body.imageUrl
+    });
+    newSample.save()
+      .then(sample => res.json(sample));
+  }
+)
+
+router.patch(
+  "/:id",
+  // passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+
+    Sample.findByIdAndUpdate((req.params.id === req.body.id), req.body, { new: true })
+      .then(sample => res.json(sample))
+      .catch(err => res.status(404).json({ nosamplefound: "No sample found by that Id" }))
+  }
+)
+
+router.delete(
+  "/:sampleId",
+  // passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Sample.deleteOne({_id: req.params.sampleId})
+      .then(() => res.status(200).json({ msg: "delete successful" }))
+      .catch(err => res.status(404).json({ nosamplefound: "No sample found by that Id" }))
+  }
+)
 
 module.exports = router;
---
+++ validation/sample.js
@@ -0,0 +1,24 @@
+const Validator = require("validator");
+const validText = require("./valid-text");
+
+module.exports = function validateSample(data) {
+  let errors = {};
+
+
+  data.sampleItem = validText(data.sampleItem) ? data.sampleItem : "";
+  data.description = validText(data.description) ? data.description : "";
+  data.price = validText(data.price) ? data.price : "";
+
+  if (Validator.isEmpty(data.sampleItem)) {
+    errors.sampleItem = "Sample item field required"
+  };
+
+  if (Validator.isEmpty(data.description)) {
+    errors.description = "Description field required"
+  };
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  };
+};
