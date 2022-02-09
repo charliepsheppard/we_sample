df75657c2eb688bae95ce1bf2b848ba702f26b8b
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-02-08 18:39:53 -0800

    first push to heroku setup

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
@@ -31,3 +32,10 @@ app.use("/api/reviews", reviews);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
---
+++ config/keys_prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://admin:wesample@wesample.x0unz.mongodb.net/wesample?retryWrites=true&w=majority',
+  secretOrKey: 'wesamplesecret'
+}
