82a364eca5e77ccf0d6179bf2fdc49c640a4d2de
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-02-12 18:20:38 -0800

    review api util and actions

---
+++ frontend/src/util/review_api_util.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export const fetchReviewsFromRestaurant = (restaurantId) => {
+  return axios.get(`/api/reviews/restaurant/${restaurantId}`)
+}
+
+export const fetchReviewsFromSample = (sampleId) => {
+  return axios.get(`/api/reviews/sample/${sampleId}`)
+}
+
+export const fetchReviewsFromUser = (userId) => {
+  return axios.get(`/api/reviews/user/${userId}`)
+}
+
+export const fetchReview = (reviewId) => {
+  return axios.get(`/api/reviews/${reviewId}`)
+}
+
+export const createReview = (review) => {
+  return axios.post(`/api/reviews/${review.stylistId}`, review)
+}
+
+export const updateReview = (review) => {
+  return axios.patch(`/api/reviews/${review.id}`, review)
+}
+
+export const deleteReview = (reviewId) => {
+  return axios.delete(`/api/reviews/${reviewId}`)
+}
