3dfcb5c502ebc9581800105ad2ec296fe7541086
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-02-16 11:22:05 -0800

    restaurnt form skeleton

--- frontend/src/components/restaurants/restaurant_form.jsx
+++ frontend/src/components/restaurants/restaurant_form.jsx
@@ -3,17 +3,51 @@ import React from 'react';
 class RestaurantForm extends React.Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            restaurantOwnerId: this.props.user.id,
+            restaurantName: '',
+            address: '',
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(field) {
         return e => {this.setState({[field] : e.currentTarget.value})}
     }
 
+     handleSubmit(e) {
+        e.preventDefault()
+        const restaurant = Object.assign({}, this.state)
+        this.props.createRestaurant(restaurant).then(() => {
+            this.setState({
+                restaurantOwnerId: this.props.user.id,
+                restaurantName: '',
+                address: ''
+            })
+        })
+        
+    }
+
+
     render() {
         return(
             <div>
                 <form className='restaurant-form'>
+                    <h3>Enter the info for you restaurant Below</h3>
 
+                    <label className='textContainer'>
+                    <label>Name
+                        <input type="text" placeholder='What is the name of your restaurant?' value={this.state.restaurantName} onChange={this.handleChange('name')}/>
+                    </label>
+
+                    <label>Address
+                        <input type="text" placeholder='Where is your restaurant located?' value={this.state.address} onChange={this.handleChange('address')}/>
+                    </label>
+
+                </label>
+                    <button onClick={this.handleSubmit}>Submit your review</button>
                 </form>
             </div>
         )
--- frontend/src/components/reviews/review_form_container.js
+++ frontend/src/components/reviews/review_form_container.js
@@ -5,7 +5,8 @@ import ReviewForm from './review_form';
 const mSTP = (state, ownProps) => {
     // console.log('state in review form container', state)
 
-    return{user: state.session.user,
+    return{
+    user: state.session.user,
     resaurant: state.entities.resaurant,
     order: ownProps.order,
     // samples: Object.values({}, state.entities.restaurant.samples)
