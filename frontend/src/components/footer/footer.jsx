a9c0588387d20c6fe779b219d5fc62893c100a25
Thiago M Moura <17tawi@gmail.com>
2022-02-17 14:56:05 -0800

    footer css

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -8,14 +8,15 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <img src={Logo} alt="" className="footer-logo-img" />
+      {/* <img src={Logo} alt="" className="footer-logo-img" /> */}
       <div className="footer-links-container">
         <Link to="/about" className="footer-link">About</Link>
         <Link to="/team" className="footer-link">Team</Link>
       </div>
       <div className="github-link">
-        <FontAwesomeIcon icon={faGithub} className="github-icon" />
+        <a href="https://github.com/charliepsheppard/we_sample" target="_blank"><FontAwesomeIcon icon={faGithub} className="github-icon" /></a>
       </div>
+        <p className="copyright">Built by Charlie Sheppard, Jack Tomasik and Thiago Moura  © 2022</p>
     </div>
   )
 }
