5a22f06205d1ec8db048f027992d58d119027993
Thiago M Moura <17tawi@gmail.com>
2022-02-17 13:54:00 -0800

    delete this

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -7,15 +7,18 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
-    <div className="footer-container">
-      <img src={Logo} alt="" className="footer-logo-img" />
-      <div className="footer-links-container">
-        <Link to="/about" className="footer-link">About</Link>
-        <Link to="/team" className="footer-link">Team</Link>
-      </div>
-      <div className="github-link">
-        <FontAwesomeIcon icon={faGithub} className="github-icon" />
-      </div>
+    <div className="social-menu">
+      {/* <img src={Logo} alt="" className="footer-logo-img" /> */}
+      {/* <div className="footer-links-container"> */}
+        <ul>
+           <li><FontAwesomeIcon icon={faGithub} className="github-icon" /></li>
+           <li><Link to="/about" className="footer-link">About</Link></li>
+           <li><Link to="/team" className="footer-link">Team</Link></li>
+          {/* </div> */}
+            {/* <div className="github-link"> */}
+            {/* </div> */}
+      </ul>
+      <p className="copyright">Built by Charlie Sheppard, Jack Tomasik and Thiago Moura  © 2022</p>
     </div>
   )
 }
