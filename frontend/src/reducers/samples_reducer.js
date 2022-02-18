444e18f77f1b308506f17eadf95fbf8c995b1700
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-02-18 10:25:09 -0800

    render restaurants samples

--- frontend/src/reducers/samples_reducer.js
+++ frontend/src/reducers/samples_reducer.js
@@ -1,13 +1,17 @@
-import {RECEIVE_SAMPLES, RECEIVE_SAMPLE } from '../actions/sample_actions';
+import {RECEIVE_SAMPLES, RECEIVE_SAMPLE, RECEIVE_RESTAURANT_SAMPLES } from '../actions/sample_actions';
 
 
-const SamplesReducer = (state = {}, action) => {
+const SamplesReducer = (state = {all: {}, restaurant: {} }, action) => {
     Object.freeze(state);
     let newState = Object.assign({}, state);
     switch(action.type) {
       case RECEIVE_SAMPLES:
+        newState.all = action.samples.data
+        return newState;
+      case RECEIVE_RESTAURANT_SAMPLES:
         console.log('Samples reducer', action)
-        return action.samples.data;
+        newState.restaurant = action.samples.data
+        return newState
       case RECEIVE_SAMPLE:
         return action.sample.data;
       default:
