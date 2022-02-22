00771b40b1180760e8ddccfb4bebfb49c7df9651
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-02-22 11:00:43 -0800

    Add delete to samples

--- frontend/src/components/sample_index/sample_index_item_container.jsx
+++ frontend/src/components/sample_index/sample_index_item_container.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createOrder } from '../../actions/order_actions';
+import { deleteSample } from '../../actions/sample_actions';
 import SampleIndexItem from './sample_index_item';
+
+const mapStateToProps = state => {
+  return {
+    sample: state.entities.sample
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return {
-    createOrder: order => dispatch(createOrder(order))
+    createOrder: order => dispatch(createOrder(order)),
+    deleteSample: sampleId => dispatch(deleteSample(sampleId)),
   };
 }
 
