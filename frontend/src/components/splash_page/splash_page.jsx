8b5699f7a350dd6ee57f096b4a500fa239172801
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-03-01 17:21:54 -0800

    Update get started link

--- frontend/src/components/splash_page/splash_page.jsx
+++ frontend/src/components/splash_page/splash_page.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import banner from './splash_page_banner.jpg';
 
 const SplashPage = () => {
@@ -11,6 +12,9 @@ const SplashPage = () => {
       <div className="splash-page-heading-container">
         <h1 className="splash-page-heading">Sample from the best around town</h1>
         {/* searchbar goes here */}
+          <Link id='get-started-link'>
+            <p>Click to get started</p>
+          </Link>
       </div>
      
     </div>
